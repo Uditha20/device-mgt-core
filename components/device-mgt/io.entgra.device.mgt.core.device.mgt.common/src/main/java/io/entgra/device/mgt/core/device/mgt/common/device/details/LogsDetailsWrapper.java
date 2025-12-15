8312b0b11387dd01971f5fd64c91a315972e23c5
Uditha20 <udithaindunil5@gmail.com>
2025-12-15 18:24:44 +0530

    Add publish data in reporting backend

---
+++ components/device-mgt/io.entgra.device.mgt.core.device.mgt.common/src/main/java/io/entgra/device/mgt/core/device/mgt/common/device/details/LogsDetailsWrapper.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2024, Entgra (Pvt) Ltd. (http://www.entgra.io) All Rights Reserved.
+ *
+ * Entgra (Pvt) Ltd. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.entgra.device.mgt.core.device.mgt.common.device.details;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+public class LogsDetailsWrapper {
+
+    @SerializedName(value = "DEVICE_ID", alternate = {"deviceId"})
+    private String deviceId;
+
+    @SerializedName(value = "TENANTID", alternate = {"tenantId"})
+    private Integer tenantId;
+
+    @SerializedName(value = "DEVICE_TYPE", alternate = {"deviceType"})
+    private String deviceType;
+
+    @SerializedName(value = "PAYLOAD", alternate = {"payload"})
+    private List<LogPayload> payload;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public Integer getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(Integer tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public List<LogPayload> getPayload() {
+        return payload;
+    }
+
+    public void setPayload(List<LogPayload> payload) {
+        this.payload = payload;
+    }
+
+    public String getJSONString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public static class LogPayload {
+
+        @SerializedName(value = "LOG_TYPE", alternate = {"logType"})
+        private String logType;
+
+        @SerializedName(value = "LOG_DATA", alternate = {"logData"})
+        private List<LogEntry> logData;
+
+        public String getLogType() {
+            return logType;
+        }
+
+        public void setLogType(String logType) {
+            this.logType = logType;
+        }
+
+        public List<LogEntry> getLogData() {
+            return logData;
+        }
+
+        public void setLogData(List<LogEntry> logData) {
+            this.logData = logData;
+        }
+    }
+
+    public static class LogEntry {
+
+        @SerializedName(value = "TIMESTAMP", alternate = {"timestamp"})
+        private Long timestamp;
+
+        @SerializedName(value = "DATA", alternate = {"data"})
+        private String data;
+
+        public Long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(Long timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    }
+}
